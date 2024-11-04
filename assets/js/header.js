c729a73f202f65f92933a16b69bebb87d2d3db33
tungdo <sont4036@gmail.com>
2024-11-04 22:36:04 +0700

    continous header/home pages

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,9 +1,31 @@
-$(".btn-search").click(function () {
-  $("#search").slideToggle("slow");
-  $(".show").slideUp("slow");
-});
+$(document).ready(function () {
+  $(document).on("click", ".btn-search", function () {
+    $("#search").slideToggle("slow");
+    $(".show").slideUp("slow");
+  });
 
-$(".col_icon-user").click(function () {
-  $(".show").slideToggle("slow");
-  $("#search").slideUp("slow");
+  $(document).on("click", ".col_icon-user", function () {
+    $(".show").slideToggle("slow");
+    $("#search").slideUp("slow");
+  });
+
+  $("#header .header_menu ul li a").click(function () {
+    $("#header .header_menu ul li a").removeClass("active"); // Xóa class 'active' khỏi các thẻ <a> khác
+    $(this).addClass("active"); // Thêm class 'active' cho thẻ <a> vừa nhấp
+  });
+
+  // Xử lý cuộn trang
+  var lastScrollTop = 0;
+  var header = $("#header");
+
+  $(window).scroll(function () {
+    var scrollTop = $(this).scrollTop();
+
+    if (scrollTop > lastScrollTop) {
+      // Cuộn xuống
+      header.addClass("fixed");
+    }
+
+    lastScrollTop = scrollTop;
+  });
 });
