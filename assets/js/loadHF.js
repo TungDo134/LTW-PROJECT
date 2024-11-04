b05a981628bd7fce40472628c224d93ab1f439e7
tungdo <sont4036@gmail.com>
2024-11-04 18:36:46 +0700

    fix footer

--- assets/js/loadHF.js
+++ assets/js/loadHF.js
@@ -1,5 +1,3 @@
 // Load header and footer content
-$(document).ready(function () {
-  $("#header-container").load("header.html");
-  $("#footer-container").load("footer.html");
-});
+$("#header-container").load("header.html");
+$("#footer-container").load("footer.html");
