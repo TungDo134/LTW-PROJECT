3b7fd926c9314bd81ea55cf25b4bfbd9da0755b2
tungdo <sont4036@gmail.com>
2024-12-03 14:46:01 +0700

    xoa folder

--- src/main/webapp/assets/js/loadHF.js
+++
@@ -1,4 +0,0 @@
-// Load header and footer content
-$("#header-container").load("header.html");
-
-$("#footer-container").load("footer.html");
--- src/main/webapp/assets/js/search.js
+++
@@ -1,8 +0,0 @@
-//// JS cho phần search ////
-// Tìm kiếm //
-$("#input-search").on("keyup", function () {
-  var value = $(this).val().toLowerCase();
-  $(".list li").filter(function () {
-    $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
-  });
-});
