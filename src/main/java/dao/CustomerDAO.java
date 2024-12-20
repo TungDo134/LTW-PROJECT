cef1080eb8689d36b563f23010f020eb2799914c
TranPhucKhai-22130109 <22130109@st.hcmuaf.edu.vn>
2024-12-20 15:17:10 +0700

    chinh sua them xoa sá»ua nguoi dung

--- src/main/java/dao/CustomerDAO.java
+++ src/main/java/dao/CustomerDAO.java
@@ -22,26 +22,62 @@ public class CustomerDAO {
     public Customer getUserByEmailPass(String email, String pass) {
         try (Handle handle = JDBIContext.getJdbi().open()) {
             return handle.createQuery("INSERT INTO customers (customerName, email, pass, phone, address, role) " +
-                    "VALUES (:customerName, :email, :pass, :phone, :address, :role)")
+                            "VALUES (:customerName, :email, :pass, :phone, :address, :role)")
                     .bind("email", email)
                     .bind("pass", pass)
                     .mapToBean(Customer.class).findOne().orElse(null);
         }
     }
 
-    public int insertCustomer(String customerName, String email, String pass, String phone, String address, String role){
+    public int insertCustomer(String customerName, String email, String pass, String phone, String address, String role) {
         Byte roler = Byte.valueOf(role);
         return JDBIContext.getJdbi().withHandle(handle ->
-            handle.createUpdate("INSERT INTO customers (customerName, email, pass, phone, address, role)\n" +
-                            "VALUES (:customerName, :email, :pass, :phone, :address, :role);")
-                    .bind("customerName", customerName)
-                    .bind("email", email)
-                    .bind("pass",pass)
-                    .bind("phone",phone)
-                    .bind("address",address)
-                    .bind("role", roler)
-                    .execute()
+                handle.createUpdate("INSERT INTO customers (customerName, email, pass, phone, address, role)\n" +
+                                "VALUES (:customerName, :email, :pass, :phone, :address, :role);")
+                        .bind("customerName", customerName)
+                        .bind("email", email)
+                        .bind("pass", pass)
+                        .bind("phone", phone)
+                        .bind("address", address)
+                        .bind("role", roler)
+                        .execute()
 
         );
     }
+
+    public int deleteUser(int customerID) {
+        return JDBIContext.getJdbi().withHandle(handle ->
+                handle.createUpdate("DELETE FROM customers WHERE customerID =:customerID")
+                        .bind("customerID", customerID)
+                        .execute()
+        );
+    }
+
+    public int updateUser(String customerName, String email, String phone, String address, String roleTxt) {
+        Byte role = Byte.valueOf(roleTxt);
+        return JDBIContext.getJdbi().withHandle(handle ->
+                handle.createUpdate("UPDATE customers set customerName = :customerName, phone =:phone, address = :address, role = :role WHERE email=:email")
+                        .bind("customerName", customerName)
+                        .bind("email", email)
+                        .bind("phone", phone)
+                        .bind("address", address)
+                        .bind("role", role)
+                        .execute()
+        );
+    }
+
+    public Customer getUserByID(String customerID) {
+        return JDBIContext.getJdbi().withHandle(handle ->
+                (handle.createQuery("select * from customers where customerID = :customerID")
+                        .bind("customerID", customerID)
+                        .mapToBean(Customer.class).one()
+                ));
+    }
+
+    public static void main(String[] args) {
+        CustomerDAO dao = new CustomerDAO();
+        dao.updateUser("Tùng Đỗ Khùng","tung134@gmail.com",
+                "0392929929", "TPHCM", "0" );
+    }
 }
+
