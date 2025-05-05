301c1fc082b0336dd3c372dd7345336af5093486
tungdo <sont4036@gmail.com>
2025-05-05 21:00:39 +0700

    Them chuc nang add user-role va kiem tra quyen khi vao chuc nang #127

--- src/main/java/controller/filter/AuthorizationFilter/ProductFilter.java
+++ src/main/java/controller/filter/AuthorizationFilter/ProductFilter.java
@@ -1,9 +1,11 @@
 package controller.filter;
 
+import entity.Customer;
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 
@@ -21,6 +23,12 @@ public class AuthorizationFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse res = (HttpServletResponse) response;
+
+        HttpSession session = req.getSession(false);
+        Customer cus = (Customer) session.getAttribute("customer");
+        if (cus == null) {
+            res.sendRedirect("/forms/login.jsp");
+        }
 
         chain.doFilter(request, response);
     }
